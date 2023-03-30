0d783253b767da0a28f2e35cc632bae97c2c7205
Vansteenacker Dylan <dylan.vansteenacker@HIGHCO.BE>
2023-03-30 14:52:47 +0200

    modify PostController + add Posts views + add phpstan config file

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class PostController extends Controller
 {
-    //
+    public function index() {
+        $posts = Post::all();
+
+        return View::make('posts.index', compact('posts'));
+    }
+
+    public function show(Post $post) {
+        $post->load('comments');
+
+        return View::make('posts.index', compact('post'));
+    }
 }
