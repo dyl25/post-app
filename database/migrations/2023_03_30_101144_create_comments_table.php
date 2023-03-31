a5aed06da72519c0563aea632390b34a65c78219
Vansteenacker Dylan <dylan.vansteenacker@HIGHCO.BE>
2023-03-31 09:40:07 +0200

    change migration file + install livewire

--- database/migrations/2023_03_30_101144_create_comments_table.php
+++ database/migrations/2023_03_30_101144_create_comments_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('post_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('post_id')->references('id')->on('posts');
             $table->text('content');
